4e445a67123290deb23517e4dd698d7556938524
akhilesh0598 <akhilesh.chaurasiya0598@gmail.com>
2022-03-15 14:51:11 +0530

    Generic Assignment 2

---
+++ GenericAssignment2/PrimeMinisters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment2
+{
+    class PrimeMinisters
+    {
+        private SortedDictionary<int, string> _primeMinisters;
+        public PrimeMinisters()
+        {
+            _primeMinisters = new SortedDictionary<int, string>();
+        }
+        public void Add(int year,string name )
+        {
+            int current_year = DateTime.Now.Year;
+            if (year < 1947 && year > current_year)
+                throw new CustomeException($"You entered wrong year\nPlease enter between 1947 to {current_year}");
+            _primeMinisters.Add(year, name);
+        }
+        public SortedDictionary<int,string>GetAll()
+        {
+            return _primeMinisters;
+        }
+        public string Get(int year)
+        {
+            if (!_primeMinisters.ContainsKey(year))
+                return "";
+            return _primeMinisters[year];
+        }
+    }
+}
---
+++ GenericAssignment2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var primeMinisters = new PrimeMinisters();
+                while (true)
+                {
+                    Console.Write("---Enter the number what you want--- ");
+                    Console.WriteLine("\n\n1 for show Prime Minister by the year");
+                    Console.WriteLine("2 for add the prime minister");
+                    Console.WriteLine("3 for show all prime minister by sorted");
+                    Console.WriteLine("Any other number for exit");
+
+                    int query = int.Parse(Console.ReadLine());
+                    switch (query)
+                    {
+                        case 1:
+                            {
+                                Console.Write("Enter the year ");
+                                int year = int.Parse(Console.ReadLine());
+                                string primeMinister=primeMinisters.Get(year);
+                                if(primeMinister=="")
+                                    Console.WriteLine("Prime Minister not Exist");
+                                else
+                                {
+                                    Console.WriteLine($"Prime Minister is = {primeMinister}");
+                                }
+                                Console.WriteLine();
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.Write("Enter the name of the prime minister ");
+                                string name = Console.ReadLine();
+                                Console.Write("Enter the year ");
+                                int year = int.Parse(Console.ReadLine());
+                                primeMinisters.Add(year, name);
+                                Console.WriteLine();
+                                break;
+                            }
+                        case 3:
+                            {
+                                var allPrimeMinisters = primeMinisters.GetAll();
+                                PrintPrimeMinister(allPrimeMinisters);
+                                Console.WriteLine();
+                                break;
+                            }
+                        default:
+                            {
+                                System.Environment.Exit(0);
+                                break;
+                            }
+
+                    }
+                }
+
+            }
+            catch(CustomeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        private static void PrintPrimeMinister(SortedDictionary<int, string> allPrimeMinisters)
+        {
+           foreach(var primeMinister in allPrimeMinisters)
+            {
+                Console.WriteLine($"Name- {primeMinister.Value} Year- {primeMinister.Key}");
+            }
+        }
+    }
+}
